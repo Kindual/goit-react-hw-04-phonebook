ee72f486baefc9aabd4d4b42e8d5d7b295733b0b
Kindual <110603179+Kindual@users.noreply.github.com>
2023-03-24 20:50:01 +0200

    complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,53 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import ContactForm from './ContactForm/ContactForm'
 import Contacts from './Contacts/Contacts';
 import Filter from './Filter/Filter';
 import Section from './Section/Section'
-import contacts from '../contacts.json'
+import contactsJson from '../contacts.json'
 
-export default class App extends Component {
-  state = {
-    contacts: localStorage.getItem('contacts') ? JSON.parse(localStorage.getItem('contacts')) : contacts,
-    filter: '',
-  }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts.length !== this.state.contacts.length) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-    }
-  }
 
-  addContact = (obj) => {
-    if (this.state.contacts.findIndex(contact => contact.name.trim().toLowerCase() === obj.name.trim().toLowerCase()) >= 0) {
+export default function App() {
+  const [contacts, setContacts] = useState(localStorage.getItem('contacts') ? JSON.parse(localStorage.getItem('contacts')) : contactsJson);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts))
+  }, [contacts]);
+
+  const addContact = (obj) => {
+    if (contacts.findIndex(contact => contact.name.trim().toLowerCase() === obj.name.trim().toLowerCase()) >= 0) {
       return false
     }
-
-    this.setState((state) =>
-      ({ ...state, contacts: [...state.contacts, obj] })
-    );
+    setContacts([...contacts, obj])
     return true
-  };
-
-  deleteContact = (id) => {
-    this.setState((state) =>
-    ({
-      ...state, contacts: state.contacts.filter(contact =>
-        contact.id !== id)
-    }))
   }
 
-  updateFilterState = (filter) => {
-    this.setState(state => ({ ...state, filter }))
+  const deleteContact = (id) => {
+    setContacts(contacts.filter(contact => contact.id !== id))
+  } 
+
+  const updateFilter = (filtr) => {
+    setFilter(filtr)
   }
 
-  render() {
-    const { filter, contacts, } = this.state;
-    const filtered = filter.trim() ? contacts.filter(contact => contact.name.toLowerCase().includes(filter.trim().toLowerCase())) : contacts;
+  const filtered = React.useMemo(
+    () => contacts.filter(contact => contact.name.toLowerCase().includes(filter.trim().toLowerCase())),
+    [filter, contacts]
+  );
 
-    return (
-      <div>
-        <Section>
-          <h1>Phonebook</h1>
-          <ContactForm addContact={this.addContact} />
-        </Section>
+  return (
+    <div>
+      <Section>
+        <h1>Phonebook</h1>
+        <ContactForm addContact={addContact} />
+      </Section>
 
-        <Section>
-          <h2>Contacts</h2>
-          <Filter updateFilterState={this.updateFilterState} filter={filter} />
-          <Contacts contacts={filtered} deleteContact={this.deleteContact} />
-        </Section>
-      </div>
-    )
-  }
+      <Section>
+        <h2>Contacts</h2>
+        <Filter updateFilterState={updateFilter} filter={filter} />
+        <Contacts contacts={filtered} deleteContact={deleteContact} />
+      </Section>
+    </div>
+  )
 }
